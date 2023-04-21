ebf97b21fda3e938bf6e2f6a1c9e068f8a37a622
gabrielwatanabe043 <gabrielpereira569@hotmail.com>
2023-04-21 17:28:18 -0300

    adicionado BOOTSTRAP

--- docs/formulario/script.js
+++ docs/formulario/script.js
@@ -2,7 +2,7 @@ function fazerpedido(){
     let nome = document.getElementById('nome').value
     let elemento = document.getElementsByName('carne');
     let checkBoxes = document.querySelectorAll('input[type="checkbox"]:checked');
-    let op, op2 = "";
+    let op, op2 = "",op3;
     for(i = 0; i < elemento.length; i++){
         if(elemento[i].checked)
         op = elemento[i].value
@@ -11,7 +11,8 @@ function fazerpedido(){
     for(i = 0; i < checkBoxes.length; i++){
         op2 = `${op2}, ${checkBoxes[i].value}`
     }
-    
-    document.getElementById('resultado').innerHTML = "Cliente: " + nome + " " + op + "G de Hamburger \n" + "Molhos: " + op2;
+    selectElement = document.querySelector('#select1')
+    op3 = selectElement.value
+    document.getElementById('resultado').innerHTML = "Cliente: " + nome + " " + op + "G de Hamburger \n" + "Molhos: " + op2 + " " + op3;
     
 }
